0c24c23b1c0a375be1644d5ce863fc6d8f1e13b0
forhadbinamir <forhadbinamir@gmail.com>
2022-03-02 15:53:42 +0300

    error added

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 const loadingSpinner = displayLoding => {
     document.getElementById('spinner').style.display = displayLoding;
 }
+// see more button 
+const seeMoreBtn = seeMore => {
+    document.getElementById('seemore').style.display = seeMore;
+}
 // clock event handler 
 document.getElementById('search-button').addEventListener('click', function () {
     const inputField = document.getElementById('input-field');
     const inputValue = inputField.value;
+    const upperCase = inputValue.toUpperCase()
+    console.log(upperCase)
     // display Spinner 
     loadingSpinner('block')
+
     if (inputField.value == "") {
         const cardField = document.getElementById('card-field');
         cardField.textContent = ''
@@ -15,10 +22,15 @@ document.getElementById('search-button').addEventListener('click', function () {
         phoneDetails.textContent = ''
         document.getElementById('warning1').style.display = 'block';
         document.getElementById('warning2').style.display = 'none';
-    }
-    else if (isNaN(inputValue) === false) {
+        document.getElementById('warning3').style.display = 'none';
+        seeMoreBtn('none')
+    } else if (isNaN(inputValue) === false) {
         document.getElementById('warning2').style.display = 'block';
         document.getElementById('warning1').style.display = 'none';
+        document.getElementById('warning3').style.display = 'none';
+        seeMoreBtn('none')
+    } else if (inputValue == upperCase) {
+        document.getElementById('warning3').style.display = 'block';
     }
     else {
 
@@ -31,13 +43,18 @@ document.getElementById('search-button').addEventListener('click', function () {
 
         document.getElementById('warning1').style.display = 'none';
         document.getElementById('warning2').style.display = 'none';
+        document.getElementById('warning3').style.display = 'none';
+
+        seeMoreBtn('none')
 
     }
 
 })
+
 // display phone with using arrow function in UI 
 const displayPhones = phones => {
     const slicephone = phones.slice(0, 20)
+    let allPhone = phones.slice(20, 400)
     const cardField = document.getElementById('card-field');
     cardField.textContent = ''
     const phoneDetails = document.getElementById('phone-details')
@@ -60,6 +77,29 @@ const displayPhones = phones => {
         `
         cardField.appendChild(div);
         loadingSpinner('none')
+        seeMoreBtn('block')
+    })
+    document.getElementById('seemore').addEventListener('click', function () {
+        allPhone.forEach(phone => {
+            // console.log(phone)
+            const div = document.createElement('div')
+            div.className = 'col-lg-4 col-md-6 col-sm-12'
+            div.innerHTML = `
+            <div class="card">
+                    <img src="${phone.image}" class="card-img-top img-fluid w-50" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${phone.brand}</h5>
+                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <p class="card-text"></p>
+                        <a href="#" onclick="showDetails('${phone.slug}')" class="btn btn-primary">Details</a>
+                    </div>
+                    
+                </div>
+            `
+            cardField.appendChild(div);
+            loadingSpinner('none')
+            seeMoreBtn('none')
+        })
     })
 
 }
@@ -94,19 +134,20 @@ const displayPhoneDetails = phoneDes => {
                         <li class="list-group-item"><span class="fw-bold">Storage: </span>${phoneDes.mainFeatures.storage}</li>
                         <li class="list-group-item">${phoneDes.mainFeatures.memory}</li>
 
-                        
                         <li class="list-group-item"><span class="fw-bold">Sensors</span></li>
                         <li class="list-group-item">${phoneDes.mainFeatures.sensors[0] ? phoneDes.mainFeatures.sensors[0] : ""}, ${phoneDes.mainFeatures.sensors[1] ? phoneDes.mainFeatures.sensors[1] : ""}, ${phoneDes.mainFeatures.sensors[2] ? phoneDes.mainFeatures.sensors[2] : ""}, ${phoneDes.mainFeatures.sensors[3] ? phoneDes.mainFeatures.sensors[3] : ""}, ${phoneDes.mainFeatures.sensors[4] ? phoneDes.mainFeatures.sensors[4] : ""}, ${phoneDes.mainFeatures.sensors[5] ? phoneDes.mainFeatures.sensors[5] : ""}, ${phoneDes.mainFeatures.sensors[6] ? phoneDes.mainFeatures.sensors[6] : ""}</li>
                         <li class="list-group-item"><span class="fw-bold">Others information</span></li>
-                        <li class="list-group-item"><span class="fw-bold">WLAN: </span>${phoneDes.others.WLAN ? phoneDes.others.WLAN : ""}</li>
-                        <li class="list-group-item"><span class="fw-bold">Bluetooth: </span>${phoneDes.others.Bluetooth ? phoneDes.others.Bluetooth : ""}</li>
-                        <li class="list-group-item"><span class="fw-bold">GPS: </span>${phoneDes.others.GPS ? phoneDes.others.GPS : ""}</li>
-                        <li class="list-group-item"><span class="fw-bold">NFC: </span>${phoneDes.others.NFC ? phoneDes.others.NFC : ""}</li>
-                        <li class="list-group-item"><span class="fw-bold">Radio: </span>${phoneDes.others.Radio ? phoneDes.others.Radio : ""}</li>
-                        <li class="list-group-item"><span class="fw-bold">USB: </span>${phoneDes.others.USB ? phoneDes.others.USB : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">WLAN: </span>${phoneDes?.others?.WLAN ? phoneDes.others.WLAN : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">Bluetooth: </span>${phoneDes?.others?.Bluetooth ? phoneDes.others.Bluetooth : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">GPS: </span>${phoneDes?.others?.GPS ? phoneDes.others.GPS : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">NFC: </span>${phoneDes?.others?.NFC ? phoneDes.others.NFC : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">Radio: </span>${phoneDes?.others?.Radio ? phoneDes.others.Radio : ""}</li>
+                        <li class="list-group-item"><span class="fw-bold">USB: </span>${phoneDes?.others?.USB ? phoneDes.others.USB : ""}</li>
                         
                     </ul>
                 </div>
     `
     phoneDetails.appendChild(div)
+    seeMoreBtn('none')
 }
+
